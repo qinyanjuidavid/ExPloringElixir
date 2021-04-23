f0396b12831e3d4b81bb61c5de0df112c069b431
qinyanjuidavid <davidkinyanjui052@gmail.com>
2021-04-23 22:05:48 +0300

    Initial Commit

---
+++ arithmeticOperators.exs
@@ -0,0 +1,36 @@
+#Arithmetic Operators
+num1=20
+num2=10
+IO.puts(num1+num2)
+IO.puts(num1-num2)
+IO.puts(num1/num2)
+IO.puts(num1*num2)
+num3=25
+IO.puts(div(num1,num3))
+IO.puts(rem(num1,num3))
+#Comparison Operators
+a="John"
+b="John"
+c="Jane"
+IO.puts(a==b)
+IO.puts(a!=b)
+IO.puts(a==c)
+IO.puts(a===b)
+IO.puts(a===c);
+IO.puts(a!==b)
+num=23
+num1=23
+num2=20
+IO.puts(num==num1)
+IO.puts(num!=num1)
+IO.puts(num==num2)
+IO.puts(num!=num2)
+IO.puts(num>num1)
+IO.puts(num>num2)
+IO.puts(num>=num1)
+IO.puts(num>=num)
+
+#Logical operators
+#and,or,not,&&,||,!
+#Bitwise
+#&&&,|||,>>>
