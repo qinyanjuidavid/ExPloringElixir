39c23caff69fed54e9061f79c2bdaa4c5bc95914
qinyanjuidavid <davidkinyanjui052@gmail.com>
2021-04-23 22:13:12 +0300

    Hands on IO.puts

---
+++ IOputs.ex
@@ -0,0 +1,15 @@
+#Out putting stuff in elixir
+IO.puts "Hello, world!"
+IO.puts "Hello friend"
+IO.puts "The best thing in this world is learning the 7yrs old language elixir.."
+IO.puts "My name is john doe..."
+IO.puts "My name is Jane Doe..."
+IO.puts "I am 27 years old..."
+IO.puts "I work in the human resource department"
+IO.puts "The earth is voidal, take care not to fall..."
+
+IO.puts("Lets check whether this method works....")
+IO.puts("Wooww the method works super nice....")
+#This is a single lined comment
+#Line endings
+IO.puts("This is line one...");IO.puts("This is line two...");IO.puts("This is line three");
