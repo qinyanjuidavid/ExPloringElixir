02a75ab2093b60cc67c954adbf305bb13017f921
qinyanjuidavid <davidkinyanjui052@gmail.com>
2021-04-28 21:28:51 +0300

    Recarp on the use of  condition

--- DecisionMaking.ex
+++ DecisionMaking.ex
@@ -54,3 +54,29 @@ unless age === 18 do
 else
     IO.puts("Can Vote.")
 end
+
+
+#Condition
+age=18
+cond do
+    age<=15 -> IO.puts("You can wait")
+    age>=16 and age<=17 -> IO.puts("You can drive.")
+    age>=18 -> IO.puts("You can vote and drive")
+end
+#Marks
+totalGrade=50
+cond do
+    totalGrade>=80 and totalGrade<=100 -> IO.puts("A")
+    totalGrade>=75 and totalGrade<=79 -> IO.puts("A-")
+    totalGrade>=69 and totalGrade<=74 -> IO.puts("B+")
+    totalGrade>=64 and totalGrade<=68 -> IO.puts("B")
+    totalGrade>=59 and totalGrade<=64 -> IO.puts("B-")
+    totalGrade>=47 and totalGrade<=53 -> IO.puts("C+")
+    totalGrade>=40 and totalGrade<=46 -> IO.puts("C")
+    totalGrade>=34 and totalGrade<=39 -> IO.puts("D+")
+    totalGrade>=29 and totalGrade<=33 -> IO.puts("D")
+    totalGrade>=24 and totalGrade<=28 -> IO.puts("D-")
+    totalGrade>=4 and totalGrade<=23 -> IO.puts("E")
+    totalGrade>=0 and totalGrade<=3 -> IO.puts("F")
+    true -> IO.puts("Invalid Marks")
+end
