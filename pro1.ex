c6522508d2768801e7631d8a01a5bd7802dfb65e
qinyanjuidavid <davidkinyanjui052@gmail.com>
2021-04-28 21:32:48 +0300

    Data Types

--- pro1.ex
+++ pro1.ex
@@ -8,3 +8,19 @@ defmodule M do
         IO.puts("My name is #{name}.")
     end
 end
+
+#Data types
+my_int=123
+IO.puts("Integer: #{my_int}")
+#Checks if the data types is an integer
+IO.puts("Integer: #{is_integer(my_int)}")
+#Floats
+my_float=3.14159
+IO.puts("Float: #{my_float}")
+IO.puts("Float: #{is_float(my_float)}")
+#Atoms
+my_atom=:Pittsburgh
+IO.puts("Atom: #{my_atom}")
+IO.puts("Atom: #{is_atom(my_atom)}")
+new_atom=:"John Doe"
+IO.puts("Atom: #{new_atom}")
